a47f7986d2e2184a87be3f581620c6ae34e80711
Fahad <fahad.js@yahoo.com>
2023-08-23 15:45:35 +0500

    Added user entity and updated application.properties file.

--- src/main/java/pk/paysenseapp/payensese_payments/entities/User.java
+++ src/main/java/pk/paysenseapp/payensese_payments/entities/User.java
@@ -1,6 +1,35 @@
 package pk.paysenseapp.payensese_payments.entities;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Table(name = "tbl_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String city;
+    private String address;
+    private String email;
+    private String accountNumber;
+    private BigDecimal accountBalance;
+    private String status;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime modifiedAt;
 }
